4c93d1ed265abc6ecafa204ee4f76edbacaf5234
svonme <svon.me@gmail.com>
2022-02-17 11:44:17 +0800

    feat:#J118-用户退出

--- src/pages/layout/user/index.vue
+++ src/pages/layout/user/index.vue
@@ -5,12 +5,12 @@
  */
 import I18n from "src/utils/i18n";
 import {Language} from "src/types/language";
-import { User } from "src/types/common/user";
+import {User} from "src/types/common/user";
 import window from "src/plugins/browser/window";
 import {createReactive} from "src/utils/ssr/ref";
-import { createHref } from "src/plugins/router/pack";
-import { config as routerConfig } from "src/router/config";
-import { languageKey, appDownload, oss } from "src/config";
+import {createHref} from "src/plugins/router/pack";
+import {config as routerConfig} from "src/router/config";
+import {languageKey, appDownload, oss} from "src/config";
 
 const i18n = I18n();
 const user = createReactive<User>("common.user", {} as User);
@@ -32,6 +32,11 @@ const onSwitch = function () {
     reload(href);
   }
 }
+
+const getUserName = function (data: User): string | number {
+  return data.nickname || data.username || data.email || data.mobile;
+}
+
 // 登录
 const goLogin = function () {
 
@@ -44,50 +49,50 @@ const goRegister = function () {
 </script>
 
 <template>
-<div class="flex items-center text-white">
+  <div class="flex items-center text-white">
   <span class="flex items-center cursor-pointer" @click.stop.prevent="onSwitch">
     <span class="inline-block whitespace-nowrap text-14-18">{{ i18n.common.lang }}</span>
   </span>
-  <span class="mx-4 text-white text-opacity-65 hidden lg:inline-block">|</span>
-  <v-router class="hidden lg:flex" :href="appDownload" target="_blank">
-    <span class="inline-block whitespace-nowrap text-14-18">APP</span>
-  </v-router>
-  <span class="mx-4 text-white text-opacity-65">|</span>
-  <div class="flex cursor-pointer user-box relative" v-if="user.id">
-    <IconFont class="flex" type="icon-yonghu1" size="22"/>
-    <div class="user-menu absolute right-0 top-full pt-2.5">
-      <div class="user-menu py-4.25 px-3 bg-global-white rounded-md">
-        <div class="flex items-center cursor-pointer">
-          <IconFont :type="`${oss}/nav/navUser.jpg`" size="20"/>
-          <div class="text-14-18 ml-2 flex whitespace-nowrap text-global-grey">
-            <template v-if="user.nickname">{{ user.nickname }}</template>
-            <template v-else-if="user.username">{{ user.username }}</template>
-            <template v-else-if="user.email">{{ user.email }}</template>
-            <template v-else>{{ user.mobile }}</template>
+    <span class="mx-4 text-white text-opacity-65 hidden lg:inline-block">|</span>
+    <v-router class="hidden lg:flex" :href="appDownload" target="_blank">
+      <span class="inline-block whitespace-nowrap text-14-18">APP</span>
+    </v-router>
+    <span class="mx-4 text-white text-opacity-65">|</span>
+    <div class="flex cursor-pointer user-box relative" v-if="user.id">
+      <IconFont class="flex" type="icon-yonghu1" size="22"/>
+      <div class="user-menu absolute right-0 top-full pt-2.5">
+        <div class="user-menu bg-global-white rounded-md">
+          <div class="p-4 flex items-center cursor-pointer">
+            <IconFont type="icon-yonghu" size="20"/>
+            <span class="ml-2 text-14-18 text-global-grey inline-block">{{ getUserName(user) }}</span>
+            <IconFont class="ml-2" type="vip1"/>
           </div>
-          <ui-image class="w-7.75 ml-2" fit="none" :src="`${oss}/nav/navVip.jpg`" alt=""/>
+          <div>
+            <span class="block border-t border-solid border-gray-300"></span>
+          </div>
+          <v-router class="p-4 flex items-center itemMt cursor-pointer" :href="routerConfig.user.logout">
+            <IconFont type="icon-tuichu" size="20"/>
+            <span class="ml-2 text-14-18 flex whitespace-nowrap text-global-grey">{{ i18n.nav.outLogin }}</span>
+          </v-router>
         </div>
-        <v-router class="mt-2 flex items-center itemMt cursor-pointer" :href="routerConfig.user.logout">
-          <IconFont :type="`${oss}/nav/navOut.jpg`" size="20"/>
-          <span class="ml-2 text-14-18 flex whitespace-nowrap text-global-grey">{{ i18n.nav.outLogin }}</span>
-        </v-router>
       </div>
     </div>
+    <div class="flex" v-else>
+      <span class="whitespace-nowrap cursor-pointer" @click.stop.prevent="goLogin">{{ i18n.common.login }}</span>
+      <img class="w-0.5 h-0.5 ml-1 mr-1 Z hidden md:inline-block" :src="`${oss}/nav/dian.png`" alt=""/>
+      <span class="whitespace-nowrap cursor-pointer hidden md:inline-block"
+            @click.stop.prevent="goRegister">{{ i18n.common.register }}</span>
+    </div>
   </div>
-  <div class="flex" v-else>
-    <span class="whitespace-nowrap cursor-pointer" @click.stop.prevent="goLogin">{{i18n.common.login }}</span>
-    <img class="w-0.5 h-0.5 ml-1 mr-1 Z hidden md:inline-block" :src="`${oss}/nav/dian.png`" alt=""/>
-    <span class="whitespace-nowrap cursor-pointer hidden md:inline-block" @click.stop.prevent="goRegister">{{i18n.common.register }}</span>
-  </div>
-</div>
 </template>
 
 <style scoped lang="scss">
 .user-box {
   .user-menu {
-    @apply hidden;
+    //@apply hidden;
     box-shadow: inset 0px -1px 0px rgba(37, 62, 111, 0.06);
   }
+
   &:hover {
     .user-menu {
       @apply block;
