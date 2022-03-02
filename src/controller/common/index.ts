e78ebf3b17fb57a68d244bc6a1716c0ae52de790
svonme <svon.me@gmail.com>
2022-03-02 10:51:00 +0800

    fix:修复重复参数引起的系统异常

--- src/controller/common/index.ts
+++ src/controller/common/index.ts
@@ -4,17 +4,53 @@
  */
 
 import _ from "lodash";
+import redirect from "./redirect";
 import I18n from "src/utils/i18n";
-import { title } from "src/config/";
-import { getTidings } from "./tiding";
+import {getTidings} from "./tiding";
 import {chainSiteConfig} from "./chain";
-import {config} from "src/router/config";
 import {userInfo, userLogout} from "./user";
-import {Router, NextFunction, Request, Response} from "express";
+import safeGet from "@fengqiaogang/safe-get";
+import safeSet from "@fengqiaogang/safe-set";
+import {Language} from "src/types/language/";
+import {languageKey, title} from "src/config/";
+import {config as routerConfig} from "src/router/config";
+import {NextFunction, Request, Response, Router} from "express";
 
 const common = Router();
+
+const getQueryLang = function (query: object) {
+	const value = safeGet<Language>(query, languageKey);
+	if (value && _.isArray(value)) {
+		const data = _.last<Language>(value);
+		if (data) {
+			return data;
+		}
+	}
+	if (value) {
+		return value;
+	}
+	return Language.auto;
+}
+
+// 处理中英文参数问题
+common.use(async function (req: Request, res: Response, next: NextFunction) {
+	const query = req.query || {};
+	const value = getQueryLang(query);
+	const lang = safeGet<string | string[]>(query, languageKey);
+	if (lang && _.isArray(lang)) {
+		redirect(req, res, req.url, { ...query, [languageKey]: value});
+	} else {
+		safeSet(req.query, value);
+		try {
+			await next();
+		} catch (e) {
+			redirect(req, res, routerConfig.E404, {[languageKey]: value})
+		}
+	}
+});
+
 // 处理用户退出逻辑
-common.all(config.user.logout, userLogout);
+common.all(routerConfig.user.logout, userLogout);
 
 // 处理公共数据
 common.use(async function (req: Request, res: Response, next: NextFunction) {
@@ -29,7 +65,7 @@ common.use(async function (req: Request, res: Response, next: NextFunction) {
 		keywords: i18n.menu.seo.key,
 		description: i18n.menu.seo.des,
 	};
-	for(const value of array) {
+	for (const value of array) {
 		_.merge(data, value);
 	}
 	Object.assign(res.locals, data);
