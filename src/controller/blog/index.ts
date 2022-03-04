20b4c7e48e0ebd6461ad05c66b4132116c391e03
yuanxuwei <yuanxuwei@jinse.com>
2022-03-04 17:56:31 +0800

    detail 动态 title key

--- src/controller/blog/index.ts
+++ src/controller/blog/index.ts
@@ -13,52 +13,52 @@ import redirect from "src/controller/common/redirect";
 import {config as routerConfig} from "src/router/config";
 
 export const list = async function (req: Request, res: Response) {
-	const i18n = I18n(req);
-	const api = new Model(req);
-	res.locals.menuActive = names.blog.blog;
-	const id = safeGet<string>(req.query, "group");
+    const i18n = I18n(req);
+    const api = new Model(req);
+    res.locals.menuActive = names.blog.blog;
+    const id = safeGet<string>(req.query, "group");
 
-	const [ list, tabs, ads, tops, hots ] = await Promise.all([
-		api.getList(id),
-		api.getTabs(),
-		api.blog.ads(),
-		api.getTopList(),
-		api.getHotList(),
-	]);
+    const [list, tabs, ads, tops, hots] = await Promise.all([
+        api.getList(id),
+        api.getTabs(),
+        api.blog.ads(),
+        api.getTopList(),
+        api.getHotList(),
+    ]);
 
-	const result = {
-		title: i18n.blog.meta.title,
-		keywords: i18n.blog.meta.keywords,
-		description: i18n.blog.meta.description,
+    const result = {
+        title: i18n.blog.meta.title,
+        keywords: i18n.blog.meta.keywords,
+        description: i18n.blog.meta.description,
 
-		"API.blog.ads": ads,      // 广告
-		[alias.blog.tabs]: tabs,    // 分组
-		[alias.blog.list]: list, // 列表数据
-		"API.blog.getTopList": tops, // 置顶数据
-		"API.blog.getHotList": hots, // 热门数据
-	};
-	res.send(result);
+        "API.blog.ads": ads,      // 广告
+        [alias.blog.tabs]: tabs,    // 分组
+        [alias.blog.list]: list, // 列表数据
+        "API.blog.getTopList": tops, // 置顶数据
+        "API.blog.getHotList": hots, // 热门数据
+    };
+    res.send(result);
 }
 
 // 博客详情
 export const detail = async function (req: Request, res: Response) {
-	const api = new Model(req);
-	const id = safeGet<number>(req.params, "id");
-	// 获取详情数据
-	const data = await api.blog.getDetail<BlogDetail>(id);
-	// 如果能获取到详情数据
-	if (data) {
-		const i18n = I18n(req);
-		res.locals.menuActive = names.blog.blog;
-		const result = {
-			title: data && data.name ? `${data.name} | KingData` : i18n.blog.meta.title,
-			keywords: i18n.blog.meta.keywords,
-			description: data.desc,
-			[alias.blog.detail]: data
-		};
-		res.send(result);
-	} else {
-		// 重定向到 404
-		redirect(req, res, routerConfig.E404);
-	}
+    const api = new Model(req);
+    const id = safeGet<number>(req.params, "id");
+    // 获取详情数据
+    const data = await api.blog.getDetail<BlogDetail>(id);
+    // 如果能获取到详情数据
+    if (data) {
+        const i18n = I18n(req);
+        res.locals.menuActive = names.blog.blog;
+        const result = {
+            title: (data && data.name) ? `${data.name} | KingData` : i18n.blog.meta.title,
+            keywords: i18n.blog.meta.keywords,
+            description: (data && data.name) ? data.desc : i18n.dapp.meta.description,
+            [alias.blog.detail]: data
+        };
+        res.send(result);
+    } else {
+        // 重定向到 404
+        redirect(req, res, routerConfig.E404);
+    }
 }
