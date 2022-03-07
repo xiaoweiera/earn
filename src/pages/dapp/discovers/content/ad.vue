ae0a5859b37de1d2eca28e84e013d57c3b28195c
zhangkun <18234114804@163.com>
2022-03-07 18:35:35 +0800

    修改广告位

--- src/pages/dapp/discovers/content/ad.vue
+++ src/pages/dapp/discovers/content/ad.vue
@@ -6,15 +6,17 @@ defineProps({
 })
 </script>
 <template>
-  <v-router class="relative w-full h-full bg-white" :href="data.url" target="_blank">
-    <div class="h-full rounded-md overflow-hidden">
-      <!-- 电脑上展示 -->
-      <ui-image class="w-full h-full" fit="cover" :src="data.image" :lazy="true"/>
-      <!-- 移动端展示 -->
-      <!-- <img class="block md:hidden w-full"> -->
-    </div>
-    <div class="absolute top-3 left-3">
-      <span class="bg-global-hei bg-opacity-45 border border-global-white border-opacity-6 rounded-md px-1.5 py-0.5 text-kd10px12px text-global-white">AD</span>
+  <v-router :href="data.url" target="_blank">
+    <div class="relative w-full h-full bg-white">
+      <div class="h-full rounded-md overflow-hidden">
+        <!-- 电脑上展示 -->
+        <ui-image class="w-full h-full" fit="cover" :src="data.image" :lazy="true"/>
+        <!-- 移动端展示 -->
+        <!-- <img class="block md:hidden w-full"> -->
+      </div>
+      <div class="absolute top-3 left-3">
+        <span class="bg-global-hei bg-opacity-45 border border-global-white border-opacity-6 rounded-md px-1.5 py-0.5 text-kd10px12px text-global-white">AD</span>
+      </div>
     </div>
   </v-router>
 </template>
