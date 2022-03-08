36fe9e69a95dd7275132813fa9b9d2e7a2f7851e
zhangkun <18234114804@163.com>
2022-03-08 14:11:02 +0800

    修改nft卡片样式

--- src/pages/dapp/nfts/footer.vue
+++ src/pages/dapp/nfts/footer.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import DappNftsTime from './time.vue';
 
-import { valueFormat } from 'src/utils/convert/to'
+import { valueFormat, toNumberFormat } from 'src/utils/convert/to'
 import { getLog } from 'src/logic/dapp'
 import I18n from "src/utils/i18n";
 const i18n = I18n();
@@ -14,23 +14,26 @@ defineProps({
 </script>
 <template>
   <div v-if="data">
-    <div class="w-full h-39.5 px-4 pt-10.5 text-global-white">
+    <div class="w-full h-39.5 px-4 pt-7 text-global-white">
       <!-- 项目名称 -->
-      <div class="flex items-center h-6">
-        <span class="max-w-40 items-center h-full text-kd20px20px short text-global-white font-semibold font-kdBarlow">{{ data.name }}</span>
+      <div class="flex items-center min-h-6 h-9">
+        <span class="max-w-full inline-flex items-center h-full text-kd16px16px text-global-white font-semibold font-kdBarlow">
+          {{ data.name }}
         <IconFont v-if="data.chain" :type="getLog(data.chain)" size="16" class="h-full items-center ml-2"/>
+        </span>
+
       </div>
       <!-- 价格总量 -->
       <div class="nft-price h-10 flex justify-between items-center border-b-1 border-global-white border-opacity-15">
         <p class="flex items-center">
           <span class="text-kd12px16px text-global-white text-opacity-65 font-kdFang">{{ i18n.home.nftProject.price }}</span>
-          <span class="text-kd16px20px font-kdBarlow ml-1 font-medium">{{valueFormat(data.mint_price)}}</span>
-          <IconFont v-if="data.chain" :type="getLog(data.chain)" size="12" class="ml-2 text-global-white"/>
+          <span class="text-kd14px20px font-kdBarlow ml-1 font-medium">12{{toNumberFormat(data.mint_price)}}</span>
+          <IconFont v-if="data.chain" :type="getLog(data.chain)" size="14" class="ml-2 text-global-white"/>
         </p>
         <p class="h-4 border-l-1 border-global-white border-opacity-15"></p>
         <p class="flex items-center">
           <span class="text-kd12px16px text-global-white text-opacity-65 font-kdFang">{{ i18n.home.nftProject.supply }}</span>
-          <span class="text-kd16px20px font-kdBarlow ml-1 font-medium">{{ valueFormat(data.issue_volume) }}</span>
+          <span class="text-kd14px20px font-kdBarlow ml-1 font-medium">{{ valueFormat(data.issue_volume) }}</span>
         </p>
       </div>
       <!-- 时间 -->
