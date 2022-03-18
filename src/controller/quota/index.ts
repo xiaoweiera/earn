b5b43cefbbe437a9b9cc040af9abebcdd14a827b
svonme <svon.me@gmail.com>
2022-03-18 12:02:54 +0800

    format:设置指标title

--- src/controller/quota/index.ts
+++ src/controller/quota/index.ts
@@ -4,24 +4,30 @@
  */
 
 import API from "src/api/";
+import I18n from "src/utils/i18n/";
 import safeGet from "@fengqiaogang/safe-get";
 import { names } from "src/config/header";
 import * as alias from "src/utils/root/alias";
 import type { Request, Response } from "express";
 import { go404 } from "src/controller/common/redirect";
+import type { Data } from "src/types/quota/";
 
 // 列表
 export const signals = async function (req: Request, res: Response) {
+  const i18n = I18n(req);
   const api = new API(req);
 
   res.locals.menuActive = names.quota.signals;
 
-  const list = await api.quota.getSignals({
+  const list = await api.quota.getSignals<Data>({
     page: 1,
     page_size: 100,
   });
 
   const data = {
+    title: i18n.news.meta.title.alert,
+    keywords: i18n.news.meta.keywords,
+    description: i18n.news.meta.description,
     [alias.quota.signals]: list,
   };
   res.send(data);
@@ -31,13 +37,18 @@ export const signals = async function (req: Request, res: Response) {
 export const detail = async function (req: Request, res: Response) {
   const id = safeGet<string>(req.params, "id");
   if (id) {
+    const i18n = I18n(req);
     const api = new API(req);
     const [data, recommend] = await Promise.all([
-      api.quota.getDetail(id), // 详情数据
-      api.quota.getRecommend(id), // 相关推荐
+      api.quota.getDetail<Data>(id), // 详情数据
+      api.quota.getRecommend<Data[]>(id), // 相关推荐
     ]);
     res.locals.menuActive = names.quota.signals;
+    const name = safeGet<string>(data, "chart.name");
     res.send({
+      title: name ? `${name} | KingData` : i18n.news.meta.title.alert,
+      keywords: i18n.news.meta.keywords,
+      description: i18n.news.meta.description,
       [alias.quota.detail]: data,
       [alias.quota.recommend]: recommend,
     });
