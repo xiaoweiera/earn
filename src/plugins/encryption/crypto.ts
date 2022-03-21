8a3102298dddd7f905840284f1f28b7a89357ada
svonme <svon.me@gmail.com>
2022-03-21 11:33:39 +0800

    fix:加密解密调整

--- src/plugins/encryption/crypto.ts
+++ src/plugins/encryption/crypto.ts
@@ -4,35 +4,24 @@
  */
 
 import { key } from "./value";
+import * as aes from "./aes";
 import * as base64 from "./base64";
-
-/*
-import CryptoJS from "crypto-js";
-
 import * as console from "src/plugins/log/index";
 
-export const Crypto = async function <T>(value: T): Promise<string> {
+const Crypto = function <T>(value: T) {
   try {
     // 将数据转换城字符串
     const jsonText = JSON.stringify({ [key]: value });
     // base64 加密
     const base64Text = base64.compress(jsonText);
     // AES 加密
-    const data = CryptoJS.AES.encrypt(base64Text, secret());
-    // 返回处理后的数据
-    return data.toString();
+    return aes.compress(base64Text);
   } catch (e) {
-    console.error("Crypto ");
+    console.error("Crypto");
     console.error(value);
     console.info(e);
   }
   return "";
-};
-*/
-
-export const Crypto = async function <T>(value: T): Promise<string> {
-  const jsonText = JSON.stringify({ [key]: value });
-  return base64.compress(jsonText);
 };
 
 export default Crypto;
