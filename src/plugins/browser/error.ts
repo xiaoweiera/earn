d4d010ad82f706393568ab77a069ef5df4441c55
svonme <svon.me@gmail.com>
2022-03-22 17:20:04 +0800

    feat:Node服务监听异常

--- src/plugins/browser/error.ts
+++ src/plugins/browser/error.ts
@@ -5,20 +5,24 @@
 
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
-import { getEnv, production } from "src/config";
+import { Command, getEnv } from "src/config";
+
+const isWatch = function (): boolean {
+  const env = getEnv();
+  // 只有线上环境才执行
+  return !!(env.VITE_command && env.VITE_command === Command.build);
+};
 
 const app = function () {
   Sentry.init({
     dsn: "https://fab97bac3bc54600962a9f6a8957c72e@o1174490.ingest.sentry.io/6270506",
     integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 5.0,
   });
 };
 
 const main = function () {
-  const env = getEnv();
-  // 只有线上环境才执行
-  if (env.VITE_name && env.VITE_name === production) {
+  if (isWatch()) {
     app();
   }
 };
