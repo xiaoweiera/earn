f904c038947c3ba208043d242d5ff29a544f87de
svonme <svon.me@gmail.com>
2022-03-24 15:47:55 +0800

    fix:判断用户传入的语种，规则外进行修正

--- src/controller/dapp/index.ts
+++ src/controller/dapp/index.ts
@@ -7,7 +7,6 @@ import { names } from "src/config/header";
 import I18n from "src/utils/i18n";
 
 export const list = async function (req: Request, res: Response) {
-  const i18n = I18n(req);
   const api = new Model(req);
   // 判断当前是否为 igo
   const is_igo = safeGet<string>(req, "query.igo") || safeGet<string>(req, "query.isIgo");
@@ -38,6 +37,7 @@ export const list = async function (req: Request, res: Response) {
     is_igo: !!is_igo,
     query: search || "",
   };
+  const i18n = I18n(req);
   const [list, summary] = await Promise.all([api.getList(projectParams), api.home.getSummary()]);
   const result = {
     "title": is_igo ? i18n.home.webIgo.title : i18n.home.webIdo.title,
