29ded1c054239aee524d0af91dad3b7924f5c8b5
yuanxuwei <yuanxuwei@jinse.com>
2022-04-07 17:57:16 +0800

    rank sort

--- src/pages/rank/dapp/item.vue
+++ src/pages/rank/dapp/item.vue
@@ -35,7 +35,6 @@ const props = defineProps({
 });
 //@ts-ignore wei
 const bgWidth = computed(() => (props.isCompare ? (props.item[props.sortName] / props.item.max_stat[props.sortName]) * 100 + "%" : ""));
-
 const detailUrl = function (data: object) {
   const category = props.category ? `category=${props.category}` : "";
   const id = safeGet<number>(data, "dapp_id");
