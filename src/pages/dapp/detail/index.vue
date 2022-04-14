64ffcc5aae247bd9c70da5e6da6d4fc3afde16d5
zhangkun <18234114804@163.com>
2022-04-14 16:15:25 +0800

    修改布局

--- src/pages/dapp/detail/index.vue
+++ src/pages/dapp/detail/index.vue
@@ -71,46 +71,48 @@ const getTabData = function (info: DAppProject, data: DAppData) {
 
 <template>
   <ui-spin class="pt-8 pb-16 px-4">
-    <div v-if="detail && detail.id" class="w-full max-w-300 mx-auto">
-      <Header :data="detail" :project="project" />
-      <ui-sticky class="mt-11 bg-white">
-        <ui-tab :def="TabName.dashboard" :list="getTabData(project, detail)" active-name="tab" @change="onChangeTab">
-          <template #default="{ data }">
-            <span class="text-18-24 font-m">{{ data.label }}</span>
+    <div class="max-w-300 mx-auto">
+      <div v-if="detail && detail.id" class="w-full">
+        <Header :data="detail" :project="project" />
+        <ui-sticky class="mt-11 bg-white">
+          <ui-tab :def="TabName.dashboard" :list="getTabData(project, detail)" active-name="tab" @change="onChangeTab">
+            <template #default="{ data }">
+              <span class="text-18-24 font-m">{{ data.label }}</span>
+            </template>
+          </ui-tab>
+        </ui-sticky>
+        <!--内容-->
+        <div :key="project.tab" class="mt-6" :data-tab="project.tab">
+          <template v-if="AnyEquals(project.tab, TabName.twitter)">
+            <Twitter :name="getTwitterName(detail)" class="bg-white" />
           </template>
-        </ui-tab>
-      </ui-sticky>
-      <!--内容-->
-      <div :key="project.tab" class="mt-6" :data-tab="project.tab">
-        <template v-if="AnyEquals(project.tab, TabName.twitter)">
-          <Twitter :name="getTwitterName(detail)" class="bg-white" />
-        </template>
-        <template v-else-if="AnyEquals(project.tab, TabName.reviews)">
-          <Reviews :id="project.id" />
-        </template>
-        <template v-else-if="AnyEquals(project.tab, TabName.nft)">
-          <NFT :data="detail" />
-        </template>
-        <template v-else-if="AnyEquals(project.tab, TabName.airdrop)">
-          <AirDrops :data="detail" />
-        </template>
-        <template v-else-if="AnyEquals(project.tab, TabName.dapp) || AnyEquals(project.tab, TabName.igo)">
-          <IDO :data="detail" :project="project" />
-        </template>
-        <template v-else>
-          <Dashboard :data="project" />
-        </template>
-      </div>
-    </div>
-    <div class="mt-4 md:mt-14">
-      <div class="flex items-center justify-between">
-        <div class="title font-kdFang">{{ i18n.home.hotRecommend }}</div>
-        <div class="flex items-center">
-          <span class="more">{{ i18n.home.lookMore }}</span>
-          <iconFont class="text-global-highTitle text-opacity-65 ml-1" type="rightNo" size="12" />
+          <template v-else-if="AnyEquals(project.tab, TabName.reviews)">
+            <Reviews :id="project.id" />
+          </template>
+          <template v-else-if="AnyEquals(project.tab, TabName.nft)">
+            <NFT :data="detail" />
+          </template>
+          <template v-else-if="AnyEquals(project.tab, TabName.airdrop)">
+            <AirDrops :data="detail" />
+          </template>
+          <template v-else-if="AnyEquals(project.tab, TabName.dapp) || AnyEquals(project.tab, TabName.igo)">
+            <IDO :data="detail" :project="project" />
+          </template>
+          <template v-else>
+            <Dashboard :data="project" />
+          </template>
         </div>
       </div>
-      <CommonTopics :isShowTitle="false" />
+      <div class="mt-4 md:mt-14">
+        <div class="flex items-center justify-between">
+          <div class="title font-kdFang">{{ i18n.home.hotRecommend }}</div>
+          <div class="flex items-center">
+            <span class="more">{{ i18n.home.lookMore }}</span>
+            <iconFont class="text-global-highTitle text-opacity-65 ml-1" type="rightNo" size="12" />
+          </div>
+        </div>
+        <CommonTopics :isShowTitle="false" />
+      </div>
     </div>
   </ui-spin>
 </template>
