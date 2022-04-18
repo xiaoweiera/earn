ceceba24098a93dab38f02c7e3fdd88f28e31028
svonme <svon.me@gmail.com>
2022-04-18 19:31:09 +0800

    format:文案微调

--- src/langs/cn/news.ts
+++ src/langs/cn/news.ts
@@ -18,7 +18,7 @@ export default {
     title: {
       quota: "KingData 指标监控",
       alert: "KingData 指标异动",
-      sub: "数据指标异动监控，快人一步获取【财富密码】",
+      sub: "数据指标异动监控，快人一步获取财富密码",
     },
     description: "KingData -- 实时关注币圈最新资讯",
     keywords: "NFT,DApp,CoinMarketCap,CoinGecko,pancakeswap,交易所,binance,defi,crypto,airdrop,空投,加密货币,巨鲸,山寨币,token",
