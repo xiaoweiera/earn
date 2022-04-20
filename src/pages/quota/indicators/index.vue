3a2f72f40b71631c66c379841fc71602424beb54
zhangkun <18234114804@163.com>
2022-04-20 12:19:57 +0800

    修改推荐指标样式

--- src/pages/quota/indicators/index.vue
+++ src/pages/quota/indicators/index.vue
@@ -30,33 +30,39 @@ const requestList = function (data: object) {
 <template>
   <div>
     <!-- 推荐指标 -->
-    <div class="w-full md:w-234 mx-auto px-4 md:px-0 pt-6 pb-6 md:pb-12 md:flex md:justify-between">
-      <div class="flex-1 pr-0 md:pr-6">
-        <!-- 头部 -->
-        <div class="px-4 hidden md:block">
-          <Tips />
-        </div>
-        <!-- 内容 -->
-        <div>
-          <ui-pagination :limit="20" skin="pagination" :init-value="getInitValue()" :request="requestList">
-            <template #default="scope">
-              <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
-                <div v-for="(item, index) in scope.list" :key="index">
-                  <IndicatorItem :data="item" />
+    <div class="w-full md:w-234 mx-auto px-4 md:px-0">
+      <ui-box>
+        <div class="flex-1 pt-6 pb-6 md:pb-12 pr-0 md:pr-6">
+          <!-- 头部 -->
+          <div class="px-4 hidden md:block">
+            <Tips />
+          </div>
+          <!-- 内容 -->
+          <div>
+            <ui-pagination :limit="20" skin="pagination" :init-value="getInitValue()" :request="requestList">
+              <template #default="scope">
+                <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
+                  <div v-for="(item, index) in scope.list" :key="index">
+                    <IndicatorItem :data="item" />
+                  </div>
                 </div>
-              </div>
-            </template>
-          </ui-pagination>
+              </template>
+            </ui-pagination>
+          </div>
         </div>
-      </div>
-      <!-- 下载-->
-      <div class="hidden md:block">
-        <ui-sticky>
-          <ui-app-download />
-        </ui-sticky>
-      </div>
+        <!--右侧 App 下载-->
+        <template #right>
+          <div class="pt-6">
+            <ui-app-download />
+          </div>
+        </template>
+      </ui-box>
     </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.ui-box {
+  --ui-box-right: 184px;
+}
+</style>
