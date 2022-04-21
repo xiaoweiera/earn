af51a02b87056092a58d30f7792d2a8efa12f389
yuanxuwei <yuanxuwei@jinse.com>
2022-04-21 12:31:34 +0800

    invest table bug

--- src/pages/layout/index.vue
+++ src/pages/layout/index.vue
@@ -8,15 +8,12 @@ import Header from "./header/index.vue";
 import Footer from "./footer/index.vue";
 import Notice from "./notice/index.vue";
 import { asyncLoad } from "src/plugins/lazyload/";
-import I18n from "src/utils/i18n";
 
 const $route = useRoute();
 
-const i18n = I18n();
 const IpValidate = asyncLoad(() => import("./notice/validate.vue"));
 const DownloadApp = asyncLoad(() => import("./download/app.vue"));
 const Tool = asyncLoad(() => import("src/components/ui/tool/index.vue"));
-const applyUrl = `/dapp/apply?lang=${i18n.getLang()}`;
 const notDownload = function (path: string): boolean {
   return !/^\/download/.test(path);
 };
@@ -40,7 +37,7 @@ const notDownload = function (path: string): boolean {
         <DownloadApp />
       </div>
       <client-only>
-        <Tool :applyUrl="applyUrl" :is-idea="true" />
+        <Tool applyUrl="/dapp/apply" :is-idea="true" />
         <!-- 判断用户来源 -->
         <IpValidate />
       </client-only>
