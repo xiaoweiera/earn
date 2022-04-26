4ebc794373b7e5b4db4f417d1809715d88f138e5
zhangkun <18234114804@163.com>
2022-04-26 12:45:54 +0800

    修改图片默认背景色

--- src/components/ui/image/index.vue
+++ src/components/ui/image/index.vue
@@ -11,6 +11,8 @@ import { getEnv } from "src/config";
 import { isLink } from "src/utils/";
 import { bgColorItem } from "src/types/ui/label";
 import { upperFirst } from "src/utils";
+import { MD5 } from "src/plugins/encryption/md5";
+import safeGet from "@fengqiaogang/safe-get";
 
 type Fit = "cover" | "contain" | "fill" | "none";
 
@@ -89,22 +91,19 @@ const index = computed<number>(function () {
   }
   return 0;
 });
-const getBgColor = function () {
-  let num = Math.floor(Math.random() * 36);
-  return `--bg-color:${bgColorItem[num]}`;
+const getBgColor = function (title: string) {
+  const value: string = MD5(title).slice(-1);
+  const color = safeGet<string>(bgColorItem, value) || bgColorItem.a;
+  return `--bg-color: ${color};`;
 };
 
 const getName = function (name: string) {
-  if (name) {
-    let text = upperFirst(name.slice(0, 2));
-    return text;
-  }
-  return "As";
+  return upperFirst(name.slice(0, 2));
 };
 </script>
 
 <template>
-  <client-only :class="className" :data-help="title" class="ui-image overflow-hidden">
+  <client-only :class="className" class="ui-image overflow-hidden">
     <template v-if="src && !error">
       <el-image :fit="getFitValue(fit)" :initial-index="index" :lazy="false" :preview-src-list="preview" :preview-teleported="true" :src="getImageLink(src, oss)" class="block w-full h-full" scroll-container="body" @error="error = true">
         <template #placeholder>
@@ -113,7 +112,7 @@ const getName = function (name: string) {
       </el-image>
     </template>
     <template v-else>
-      <span v-if="title" :style="getBgColor()" class="back-color flex items-center justify-center w-full h-full">
+      <span v-if="title" :style="getBgColor(title)" class="back-color">
         {{ getName(title) }}
       </span>
       <el-image v-else :fit="getFitValue(fit)" :lazy="false" :src="getImageLink()" class="block w-full h-full" />
@@ -123,6 +122,7 @@ const getName = function (name: string) {
 <style lang="scss" scoped>
 @import "src/styles/function";
 .back-color {
+  @apply flex items-center justify-center w-full h-full text-white tracking-wider;
   background: linear-gradient(180deg, rgba(255, 255, 255, 0.3) 0%, rgba(0, 0, 0, 0.1) 100%), var(--bg-color);
 }
 
@@ -131,6 +131,7 @@ const getName = function (name: string) {
   @at-root .kd-ui-icon & {
     @apply overflow-hidden;
   }
+  /*
   &.help {
     @apply cursor-pointer;
     &:after {
@@ -149,5 +150,6 @@ const getName = function (name: string) {
       }
     }
   }
+   */
 }
 </style>
--- src/pages/dapp/investment/projects/item.vue
+++ src/pages/dapp/investment/projects/item.vue
@@ -33,16 +33,16 @@ const getCount = function (data: number) {
   <div class="min-h-94.5 p-1.5 bg-global-white border-1 border-global-highTitle border-opacity-6 rounded-md">
     <v-router :href="`${config.funds}/${data.project.id}`" target="_blank" name="div">
       <!-- 头部 -->
-      <div class="w-full">
-        <p class="w-full h-11.25 rounded-md bg-global-numGreen bg-opacity-12 flex items-center justify-center">
+      <div>
+        <p class="h-11.25 rounded-md bg-global-numGreen bg-opacity-12 flex items-center justify-center">
           <span class="text-kd24px24px text-global-numGreen font-semibold font-kdBarlow">{{ data.stage_name }}</span>
         </p>
       </div>
       <!-- logo -->
-      <div class="w-full mt-3">
+      <div class="mt-3">
         <div class="w-24 h-24 mx-auto relative">
-          <div class="w-full h-full">
-            <ui-image class="w-full h-full" rounded fit="cover" :src="data.project.logo"></ui-image>
+          <div class="h-full">
+            <ui-image class="h-full text-24-28" rounded fit="cover" :src="data.project.logo" :title="data.project.name"></ui-image>
           </div>
           <div v-if="data.project.categories && data.project.categories.length > 0" class="w-full h-6.5 flex justify-center absolute left-0 bottom-0">
             <p class="w-max flex px-2 pt-1 py-1.75 bg-global-numGreen border-2 border-global-white rounded-2xl">
@@ -52,22 +52,22 @@ const getCount = function (data: number) {
         </div>
       </div>
       <!-- name -->
-      <div class="w-full mt-1.5">
-        <p class="w-full text-center">
+      <div class="mt-1.5">
+        <p class="text-center">
           <span class="text-kd24px28px text-global-highTitle font-kdBarlow line-clamp-1">{{ data.project.name }}</span>
         </p>
       </div>
       <!-- 总数 -->
-      <div class="w-full mt-2">
-        <p class="w-full text-center">
+      <div class="mt-2">
+        <p class="text-center">
           <span class="text-kd14px18px text-global-highTitle text-opacity-65 font-medium font-kdFang">{{ i18n.invest.project.total }}</span>
           <span class="mx-1 text-kd20px20px text-global-highTitle text-opacity-85 font-semibold font-kdBarlow">{{ data.investors.length }}</span>
           <span class="text-kd14px18px text-global-highTitle text-opacity-65 font-medium font-kdFang">{{ i18n.invest.project.investors }}</span>
         </p>
       </div>
       <!-- 参与logo -->
-      <div :class="{ overlap: data.investors.length > 9 }" class="w-full mt-2 px-1.5 group-list">
-        <div class="w-full min-h-11.5 py-2 pl-1.5 border-t-1 border-b-1 border-global-highTitle border-opacity-6 flex justify-center items-center">
+      <div :class="{ overlap: data.investors.length > 9 }" class="mt-2 px-1.5 group-list">
+        <div class="min-h-11.5 py-2 pl-1.5 border-t-1 border-b-1 border-global-highTitle border-opacity-6 flex justify-center items-center">
           <template v-for="(item, index) in data.investors" :key="item.id">
             <v-router class="block p-0.5 group-item bg-global-white rounded-1/2" :style="`z-index:${index}`" :href="`${config.invest}/${item.id}`" target="_blank">
               <ui-image class="w-6 h-6 font-kdInter text-kd12px16px text-global-white" rounded fit="cover" :src="item.logo" :title="item.name" />
