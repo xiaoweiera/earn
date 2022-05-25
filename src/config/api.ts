20a917f264ff85940f7797b6944178ed2c838088
zhangkun <18234114804@163.com>
2022-05-25 22:50:53 +0800

    详情页

--- src/config/api.ts
+++ src/config/api.ts
@@ -19,7 +19,8 @@ export const home = {
 };
 
 export const dapp = {
-  detail: "/api/v1/dapps/detail", // 项目库详情
+  // detail: "/api/v1/dapps/detail", // 项目库详情
+  detail: "/api/v1/dapp/projects/{id}", // 项目库详情
   list: "/api/v1/dapps/ido", // IDO项目列表
   igoList: "/api/v1/dapps/igo", // IGO项目列表
   nftList: "/api/v1/dapps/nft", // nft项目列表
