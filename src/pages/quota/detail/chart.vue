08cce6546b2dd9292750590de1baaaca11c03909
svonme <svon.me@gmail.com>
2022-05-27 17:50:33 +0800

    fix:debug-8

--- src/pages/quota/detail/chart.vue
+++ src/pages/quota/detail/chart.vue
@@ -24,7 +24,7 @@ const i18n = I18n();
         <icon-vip class="ml-1" :type="data.chart_type" />
       </v-router>
       <div class="ml-2">
-        <!--        <OnFollow :id="data.id" v-model:status="data.followed" />-->
+        <OnFollow :id="data.id" v-model:status="data.followed" />
       </div>
     </div>
     <div v-if="data.desc" class="mt-1">
@@ -32,12 +32,12 @@ const i18n = I18n();
         <ui-markdown class="text-12-20" :value="data.desc" />
       </ui-description>
     </div>
-    <div class="mt-1.5 flex items-center text-global-text-grey">
-      <IconFont type="icon-users" size="16" />
-      <span class="ml-1 text-12-16">{{ toNumberCash(data.follow_count) }}{{ i18n.news.detail.follow }}</span>
-      <IconFont class="ml-3" type="icon-view" size="16" />
-      <span class="ml-1 text-12-16">{{ toNumberCash(data.view_count) }}</span>
-    </div>
+    <!--    <div class="mt-1.5 flex items-center text-global-text-grey">-->
+    <!--      <IconFont type="icon-users" size="16" />-->
+    <!--      <span class="ml-1 text-12-16">{{ toNumberCash(data.follow_count) }}{{ i18n.news.detail.follow }}</span>-->
+    <!--      <IconFont class="ml-3" type="icon-view" size="16" />-->
+    <!--      <span class="ml-1 text-12-16">{{ toNumberCash(data.view_count) }}</span>-->
+    <!--    </div>-->
   </div>
 </template>
 
