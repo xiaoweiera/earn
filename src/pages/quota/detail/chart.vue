28d304641456c743a34d601599380828516e0ac1
svonme <svon.me@gmail.com>
2022-05-27 17:59:14 +0800

    fix:debug-9

--- src/pages/quota/detail/chart.vue
+++ src/pages/quota/detail/chart.vue
@@ -28,16 +28,16 @@ const i18n = I18n();
       </div>
     </div>
     <div v-if="data.desc" class="mt-1">
-      <ui-description :line="3" line-height="20px">
-        <ui-markdown class="text-12-20" :value="data.desc" />
-      </ui-description>
+      <!--      <ui-description :line="3" line-height="20px">-->
+      <!--        <ui-markdown class="text-12-20" :value="data.desc" />-->
+      <!--      </ui-description>-->
     </div>
-    <!--    <div class="mt-1.5 flex items-center text-global-text-grey">-->
-    <!--      <IconFont type="icon-users" size="16" />-->
-    <!--      <span class="ml-1 text-12-16">{{ toNumberCash(data.follow_count) }}{{ i18n.news.detail.follow }}</span>-->
-    <!--      <IconFont class="ml-3" type="icon-view" size="16" />-->
-    <!--      <span class="ml-1 text-12-16">{{ toNumberCash(data.view_count) }}</span>-->
-    <!--    </div>-->
+    <div class="mt-1.5 flex items-center text-global-text-grey">
+      <IconFont type="icon-users" size="16" />
+      <span class="ml-1 text-12-16">{{ toNumberCash(data.follow_count) }}{{ i18n.news.detail.follow }}</span>
+      <IconFont class="ml-3" type="icon-view" size="16" />
+      <span class="ml-1 text-12-16">{{ toNumberCash(data.view_count) }}</span>
+    </div>
   </div>
 </template>
 
