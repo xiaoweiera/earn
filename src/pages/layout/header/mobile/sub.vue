2c2c1daeb8be82daadf2e61eea24c6f541696883
svonme <svon.me@gmail.com>
2022-05-29 19:57:52 +0800

    feat:移动端菜单

---
+++ src/pages/layout/header/mobile/sub.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+/**
+ * @file 三级子导航
+ * @auth svon.me@gmail.com
+ */
+
+import type { PropType } from "vue";
+import type { MenuItem } from "src/types/menu/";
+
+defineProps({
+  list: {
+    type: Array as PropType<MenuItem[]>,
+    default() {
+      return [];
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <template v-for="item in list" :key="item.name">
+      <v-router v-if="!item.group" :href="item.href" class="block" :class="{ active: item.active }">
+        <span class="item-name">{{ item.name }}</span>
+      </v-router>
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.item-name {
+  @apply text-kd14px18px text-global-highTitle font-medium font-kdFang text-opacity-65;
+}
+
+.active {
+  .item-name {
+    @apply text-global-darkblue;
+  }
+}
+</style>
