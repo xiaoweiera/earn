2d5560db438f38a1a820c02e79134a9a01f8f2f3
Byronlee <byronlee@192.168.0.100>
2022-06-21 17:04:33 +0800

    hotfix

--- src/pages/freemint/lib/nft.js
+++ src/pages/freemint/lib/nft.js
@@ -11,10 +11,6 @@ class Nft {
     this.ws_web =  this.Web3.createAlchemyWeb3(this.ws_alchemy_url)
   }
 
-  ayync get_lastest_mint_tx() {
-    // thi
-  }
-
   /*
     // const rpc_list = [
     //   "https://mainnet-eth.compound.finance/",
@@ -60,12 +56,96 @@ class Nft {
     return await mycontract.methods[method_name]().call({ from: "0x67d9417c9c3c250f61a83c7e8658dac487b56b09", gasPrice: "0" })
   }
 
+
+
+  /*
+    mint_prams = {
+      from: '',
+      to: ''.
+      gasLimit: '',
+      value: '',
+      data: '',
+
+      baseFeePerGas: 10
+      maxPriorityFeePerGas: 20,
+      maxFeePerGas: 30,
+
+    }
+  */
+
+  async mint_nft(mint_params, privateKeys, logs){
+    logs.push({ state: 'true', msg: '参数解析中...'})
+
+    privateKeys.map(privateKey => {
+      let address = await this.api_web3.eth.accounts.privateKeyToAccount(privateKey).address
+      let balance = await this.getBalance(address)
+      let values = await Number(web3.utils.fromWei(balance ? balance : '')).toFixed(3)
+
+      let txParams = {
+          data: inputStr,
+          value: web3.utils.toWei(tx.value.toString(), 'ether'),
+          maxFeePerGas: web3.utils.toHex(tx.maxFeePerGas * 10 ** 9), //wei
+          maxPriorityFeePerGas: web3.utils.toHex(tx.maxPriorityFeePerGas * 10 ** 9), //wei
+      }
+      console.log('txParams: ', txParams)
+
+      let mySignTransaction = await this.api_web3.eth.accounts.signTransaction(txParams, privateKey)
+      console.log('mySignTransaction: ', mySignTransaction)
+
+      let sendSignedTransaction = this.api_web3.eth.sendSignedTransaction(mySignTransaction, function (err, address) {
+          console.log('err, address: ', err, address)
+          if (!err) console.log(address) // "0x7f9fade1c0d57a7af66ab4ead7c2eb7b11a91385"
+      })
+      console.log('sendSignedTransaction: ', sendSignedTransaction)
+      logs.push({ state: 'true', msg: sendSignedTransaction})
+    })
+  }
+
+
+  //获取余额
+  async getBalance(address) {
+    return await web3.eth.getBalance(address, 'latest')
+  }
+
+  // 根据 tx hash 或者 合约地址去识别 mint 相关参数
+  async auto_parse_mint(hash) {
+    let mint_params =  {
+      input_data: '0xefef39a10000000000000000000000000000000000000000000000000000000000000032',
+      mint_number: 1,
+      value: 0
+    }
+
+    switch this._parse_hash_type(hash) {
+      case 'NFT_CONTRACT_HASH' {
+
+      }
+      case "NFT_MINT_TX_HASH" {
+
+      } final {
+        return alert("The entered hash is neither NFT Mint Hash nor NFT contract address Hash, please re-enter !")
+      }
+    }
+    return mint_params;
+  }
+
+  async _parse_hash_type(hash) {
+    return 'NFT_MINT_TX_HASH';
+  }
+
   group_by_block(nft_events) {
     // TODO
   }
 
   group_by_collection(nft_events) {
     // TODO
+  }
+
+  ayync get_lastest_mint_tx() {
+    lastBlock = this.api_web3.eth.getBlockNumber()
+    txs = await his.fetch_nft_mint_transactions(lastBlock - 10000000, lastBlock)
+    return txs.map(tx => {
+      return formate_nft(tx)
+    })
   }
 
   async fetch_nft_mint_transactions(from_block, to_block) {
@@ -81,83 +161,6 @@ class Nft {
       excludeZeroValue:false,
       category: ["erc721","erc1155"]
     })
-  }
-
-  /*
-    mint_prams = {
-
-    }
-  */
-
-  async mint_nft(mint_params) {
-    // Constructing the raw transaction
-    // To 'Speed Up' a transaction like Metamask just send a new transaction with the same nonce but with higher gas
-    // const rawTx = {
-    //     "from"                  :   web3.utils.toHex( sendingAddress ),
-    //     "to"                    :   web3.utils.toHex( receivingAddress ),
-    //     "gasLimit"              :   web3.utils.toHex( 210000 ),
-    //     "maxFeePerGas"          :   web3.utils.toHex( web3.utils.toWei( '1.5' , 'gwei' ) ),
-    //     "maxPriorityFeePerGas"  :   web3.utils.toHex( web3.utils.toWei( '1.5' , 'gwei' ) ),
-    //     "value"                 :   web3.utils.toHex( web3.utils.toWei( '0.1' , 'ether' ) ),
-    //     "data"                  :   web3.utils.toHex( 'This is my example' ),
-    //     "nonce"                 :   web3.utils.toHex( await web3.eth.getTransactionCount( sendingAddress , 'pending' ) ),
-    //     "chainId"               :   "0x04",
-    //     "accessList"            :   [],
-    //     "type"                  :   "0x02"
-    // };
-    // baseFeePerGas (base fee)
-    // maxPriorityFeePerGas (priority fee) –
-    // maxFeePerGas (max fee)
-
-    // // Creating a new transaction
-    //  const tx = FeeMarketEIP1559Transaction.fromTxData( rawTx , { chain } );
-    //
-    //  // Signing the transaction
-    //  const signedTransaction = tx.sign( privateKey );
-    //
-    //  // Generating transaction Hash
-    //  const txHash = await web3.utils.sha3( '0x' + signedTransaction.serialize().toString( 'hex' ) );
-    //  console.log( "Tx Hash: " + txHash );
-    //
-    //  // Sending transaction
-    //  await web3.eth.sendSignedTransaction( '0x' + signedTransaction.serialize().toString( 'hex' ) )
-    //  .on( 'error' , function( error ) {
-    //      console.error( error )
-    //  } );
-    //
-    //  console.log( 'After Transaction' );
-    //  sendingAddressBalance = await web3.eth.getBalance( sendingAddress );
-    //  console.log( "Address: " + sendingAddress );
-    //  console.log( "Balance: " + sendingAddressBalance );
-    //  console.log( Array( 75 ).join( "=" ) );
-    //  console.log( " " );
-
-  }
-  // 根据 tx hash 或者 合约地址去识别 mint 相关参数
-  async auto_recognition_mint(hash) {
-    let mint_params =  {
-      input_data: '',
-      gas_price: '',
-      gas_limit: '',
-      mint_num: 4,
-      value: 3
-    }
-
-    switch parse_hash_type(hash) {
-      case 'NFT_CONTRACT_HASH' {
-
-      }
-      case "NFT_MINT_TX_HASH" {
-
-      } final {
-        return alert("输入的 hash 既不是 NFT Min Hash 也不是 NFT 合约地址 Hash, 请重新输入 !")
-      }
-    }
-    return mint_params;
-  }
-
-  async parse_hash_type(hash) {
-
   }
 
   formate_nft(nft_event) {
